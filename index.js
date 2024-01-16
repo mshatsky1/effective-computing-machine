1adf2dd0a89c6615f986da96a2d10dd7714a4593
mshatsky1 <45709544+mshatsky1@users.noreply.github.com>
2024-01-16 11:45:00 +0200

    Integrate user routes into main app

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const userRoutes = require('./routes/users');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
@@ -7,6 +8,8 @@ app.use(express.json());
 app.get('/', (req, res) => {
   res.json({ message: 'Hello World' });
 });
+
+app.use('/api/users', userRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
