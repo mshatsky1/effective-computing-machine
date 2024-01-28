27d3b746726c508aebb94d73840a516c5da4d7a5
mshatsky1 <45709544+mshatsky1@users.noreply.github.com>
2024-01-28 14:20:00 +0200

    Integrate request logger middleware

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const userRoutes = require('./routes/users');
 const { errorHandler, notFoundHandler } = require('./middleware/errorHandler');
+const requestLogger = require('./middleware/logger');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(requestLogger);
 app.use(express.json());
 
 app.get('/', (req, res) => {
