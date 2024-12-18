ef4172bb94028d56c275f05582eca9d301138cc8
mshatsky1 <45709544+mshatsky1@users.noreply.github.com>
2024-12-18 13:45:00 +0200

    Refactor request ID to use simple ID generation without external dependency

--- middleware/requestId.js
+++ middleware/requestId.js
@@ -1,7 +1,8 @@
-const { v4: uuidv4 } = require('uuid');
-
 function requestIdMiddleware(req, res, next) {
-  req.id = uuidv4();
+  // Generate a simple request ID using timestamp and random number
+  const timestamp = Date.now();
+  const random = Math.random().toString(36).substring(2, 9);
+  req.id = `${timestamp}-${random}`;
   res.setHeader('X-Request-ID', req.id);
   next();
 }
