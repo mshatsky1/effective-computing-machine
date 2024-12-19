4301c98779e77b8c28c4df3f8f4feb4ac0f4dd9e
mshatsky1 <45709544+mshatsky1@users.noreply.github.com>
2024-12-19 11:15:00 +0200

    Enhance logger: include request ID in log output

--- middleware/logger.js
+++ middleware/logger.js
@@ -1,11 +1,12 @@
 function requestLogger(req, res, next) {
   const start = Date.now();
   const originalSend = res.send;
+  const requestId = req.id || 'unknown';
   
   res.send = function(data) {
     const duration = Date.now() - start;
     const statusCode = res.statusCode || 200;
-    console.log(`${req.method} ${req.path} - ${statusCode} - ${duration}ms`);
+    console.log(`[${requestId}] ${req.method} ${req.path} - ${statusCode} - ${duration}ms`);
     originalSend.call(this, data);
   };
   
