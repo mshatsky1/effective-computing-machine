e2d0ef05a256bf9e53674826ebe52f65475356c0
mshatsky1 <45709544+mshatsky1@users.noreply.github.com>
2025-12-18 09:42:22 +0200

    Improve response time calculation precision

--- middleware/responseTime.js
+++ middleware/responseTime.js
@@ -9,7 +9,8 @@ function responseTime(req, res, next) {
   res.end = function patchedEnd(...args) {
     const durationNs = process.hrtime.bigint() - start;
     const durationMs = Number(durationNs) / 1e6;
-    res.setHeader('X-Response-Time', `${durationMs.toFixed(2)}ms`);
+    const roundedMs = Math.round(durationMs * 100) / 100;
+    res.setHeader('X-Response-Time', `${roundedMs.toFixed(2)}ms`);
     originalEnd.apply(this, args);
   };
 
